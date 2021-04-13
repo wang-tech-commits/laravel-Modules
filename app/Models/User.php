8918051c523d940af72b9511c817a32aa903253b
wangxiaoyu <15124532157@163.com>
2021-04-13 11:52:51 +0800

    [调整] 用户模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use Encore\Admin\Traits\DefaultDatetimeFormat;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Jason\Api\Traits\ApiGuardable;
+use Modules\User\Entities\UserInfo;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use Notifiable, ApiGuardable;
+    use Notifiable, ApiGuardable, DefaultDatetimeFormat;
 
     protected $guarded = [];
 
@@ -29,4 +32,22 @@ class User extends Authenticatable implements JWTSubject
      */
     protected $casts = [
     ];
+
+    public function info()
+    {
+        return $this->hasOne(UserInfo::class);
+    }
+
+    /**
+     * 当用户有密码的时候才加密
+     * @Author:<Mr.Wang>
+     * @Date:2021-04-12
+     * @param [type] $password [description]
+     */
+    public function setPasswordAttribute($password)
+    {
+        if ($password) {
+            $this->attributes['password'] = bcrypt($password);
+        }
+    }
 }
